c26c1b8f7dc035597cbea2ce9f1ccf570cda6cb0
archit singh <117028669+architsingh1609@users.noreply.github.com>
2022-12-01 22:36:32 +0530

    Add files via upload

---
+++ module1.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      archit singh
+#
+# Created:     25-11-2022
+# Copyright:   (c) archit singh 2022
+# Licence:     <your licence>
+#--------------------------------------------------------------------------
+
+print("hi archit bhai ")
+
